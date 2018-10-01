2fa7ebd8dd8f3d55b36f4f7f2d7a2347df6f2890
lassenp <philiplassen@gmail.com>
2018-10-01 02:52:33 +0200

    solutions to a, c, and d, with d depending on an incomplete solution to b.

--- 3/db.pl
+++ 3/db.pl
@@ -4,10 +4,29 @@ g1([person(kara, [barry, clark]),
  person(clark, [oliver, kara]),
  person(oliver, [kara])]).
 
-
-likes([person(X, Friends)|Tail], X, Y) :- member(Y, Friends).
+mem(Y, [Y| Tail]).
+mem(Y, [Head | Tail]) :- mem(Y, Tail).
+likes([person(X, Friends)|Tail], X, Y) :- mem(Y, Friends).
 likes([Head | Tail], X, Y) :- likes(Tail, X, Y).
 
-different(G, X, Y) :- member(person(X, _), G), member(person(Y, _), G), Y \= X.
+/**
+* X likes Y, Y no likes X, Y in X
+**/
 
-dislikes(G, X, Y) :- different(G, X, Y), likes(G, Y, X), not (likes(G, X, Y)).
+
+
+popular(G, X) :- pop1(G, X, G).
+pop1([person(X, Friends)|Tail], X, G):- allLike(G, X, Friends).
+pop1([Head | Tail], X, G) :- pop1(Tail, X, G).
+allLike(G, X, [Head | Tail]) :- likes(G, Head, X), allLike(G, X, Tail).
+allLike(G, X, []).
+
+outcast(G, X) :- out1(G, X, G).
+out1([person(X, Friends)|Tail], X, G) :- allDisLike(G, X, Friends).
+out1([Head | Tail], X, G) :- out1(Tail, X, G).
+allDisLike(G, X, [Head | Tail]) :- dislikes(G, Head, X), allDisLike(G, X, Tail).
+allDisLike(G, X, []). 
+
+
+
+
