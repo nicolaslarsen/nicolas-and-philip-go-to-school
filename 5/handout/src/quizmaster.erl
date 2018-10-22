e5879938c326f7c433d22a776a97cacbb3dd281a
nicolas <nicolasringsmose@hotmail.com>
2018-10-22 08:35:13 +0200

    A lot of stuff is working, still needs fine-tuning

--- 5/handout/src/quizmaster.erl
+++ 5/handout/src/quizmaster.erl
@@ -2,30 +2,31 @@
 
 %% API exports.
 -export([start/0, add_question/2, get_questions/1,
-         play/1, next/1, timesup/1, loop/3,
-         playerscore/1, hideAnswers/1, addToTotal/2,
-         name_free/2, join/2, leave/2, guess/3]).
+         play/1, next/1, timesup/1, loop/3, get_dist/2,
+         inc_nth/2, playerscore/1, hideAnswers/1, addToTotal/2,
+         player_free/2, join/2, leave/2, guess/3]).
+
 loop(Questions, Players, Status) ->
     Me = self(),
     receive
         {From, {add_question, Description, MarkedAnswers}} ->
-            if
-              Status =:= editable ->
+            case Status of
+              editable ->
                 From ! {Me, ok},
                 loop(queue:snoc(Questions, {Description, MarkedAnswers}), Players, Status);
-              true -> From ! {error, we_are_playing},
+              _ -> From ! {error, we_are_playing},
                       loop(Questions, Players, Status)
             end;
         {From, get_questions} ->
             From ! {Me, {questions, Questions}},
             loop(Questions, Players, Status);
         {From, play} ->
-                    if
-                      Status =:= editable ->
+                    case Status of
+                      editable ->
                         From ! {Me, ok},
                         loop(Questions, Players,
                           {From, playing_between_questions, playerscore(Players)});
-                      true -> 
+                      _ ->
                         From ! {Me, {error, conductor_exists}},
                         loop(Questions, Players, Status)
                     end;
@@ -39,31 +40,52 @@ loop(Questions, Players, Status) ->
 
                             % Sends next_question to player, to be used with map
                             SendToPlayer =
-                              fun(Pid, {_, Ref}) ->
+                              fun(Ref, {_, Pid}) ->
                                       Pid ! {next_question, Ref, Question}
                               end,
 
                             maps:map(SendToPlayer, Players),
                             loop(Questions, Players,
-                                 {Conductor, playing_active_question, Scores});
+                                 {Conductor, {playing_active_question, #{}}, Scores});
                           true ->
                             From ! {error, who_are_you},
                             loop(Questions, Players, Status)
                         end;
-                      {_, playing_active_question, _} ->
+                      {_, {playing_active_question, _} , _} ->
                           From ! {Me, {error, has_active_question}},
+                          loop(Questions, Players, Status);
+                      _ ->
+                          From ! {Me, {error, not_playing_yet}},
                           loop(Questions, Players, Status)
                     end;
+            % TODO: At least end the game?
         {From, times_up} ->
                     case Status of
-                      {Conductor, playing_active_question, Scores} ->
+                      {Conductor, {playing_active_question, Guesses}, Scores} ->
                         if From =:= Conductor ->
-                          LastQ = playerscore(Players), %standing for LastQ
-                          NewTotal = addToTotal(maps:to_list(LastQ), Scores),
-                          % We still need to remove placeholders
-                          From ! {Me, {ok, dist_undefined, LastQ, NewTotal, false}},
-                          loop(Questions, Players,
-                               {Conductor, playing_between_questions, NewTotal});
+                          case queue:out(Questions) of
+                            {{_, {_, Answers}}, RemQuestions} ->
+
+                              % Give points for a single guess
+                              GivePoints = 
+                                fun(_, Index) -> 
+                                    case lists:nth(Index, Answers) of
+                                        {correct, _}  -> 1;
+                                        _             -> 0
+                                    end
+                                end,
+
+                              Dist = get_dist(Guesses, length(Answers)),
+                              LastQ = maps:map(GivePoints, Guesses),
+                              NewTotal = addToTotal(maps:to_list(LastQ), Scores),
+                              Final = queue:is_empty(RemQuestions) =:= true,
+
+                              % We still need to remove placeholders
+                              From ! {Me, {ok, Dist, LastQ, NewTotal, Final}},
+                              loop(RemQuestions, Players,
+                                {Conductor, playing_between_questions, NewTotal});
+                            _ -> From ! {Me, {error, question_wrong_format}}
+                          end;
                         true ->
                           From ! {Me, {error, nice_try}},
                           loop(Questions, Players, Status)
@@ -76,25 +98,44 @@ loop(Questions, Players, Status) ->
                     end;
         {From, join, Player} ->
                     case Status of
-                      {editable} ->
-                            NameIsFree = name_free(Player, maps:values(Players)),
+                      editable ->
+                            NameIsFree = player_free(Player, maps:values(Players)),
                             if
                               NameIsFree ->
                                     Ref = make_ref(),
-                                    NewPlayers = maps:put(From, {Player, Ref}),
+                                    NewPlayers = maps:put(Ref, {Player, From}, Players),
                                     From ! {Me, {ok, Ref}},
                                     loop(Questions, NewPlayers, editable);
-                              true -> 
+                              true ->
                                     From ! {Me, {error, is_taken}},
                                     loop(Questions, Players, Status)
                             end;
-                      _ -> 
+                      _ ->
                           From ! {Me, {error, cant_join_yet}},
                           loop(Questions, Players, Status)
                     end;
+        % Players does not have to be deleted from the scoreboard,
+        % so there's no reason to check the status
+        {From, leave, Ref} ->
+                    From ! {Me, ok},
+                    loop(Questions, maps:remove(Ref, Players), Status);
+        {From, guess, Ref, Index} ->
+            case Status of
+              {Conductor, {playing_active_question, Guesses}, Scores} ->
+                PlayerExists = maps:is_key(Ref, Players),
+                if
+                  PlayerExists ->
+                    loop(Questions, Players,
+                         {Conductor,
+                          {playing_active_question, maps:put(Ref, Index, Guesses)},
+                           Scores})
+                end;
+              _ -> loop(Questions, Players, Status) % We just ignore the guess
+            end;
         {From, _} -> From ! {Me, {error, "Arguments are on the wrong form"}},
                      loop(Questions, Players, Status)
     end.
+
 
 % We get the keys already defined in Players, then map each key to a {key, 0}.
 % This way our scoreboard is initialized. Then we convert this new list to a map.
@@ -102,32 +143,45 @@ playerscore(Players) ->
         Keys = maps:keys(Players),
         maps:from_list(lists:map(fun(X) -> {X, 0} end, Keys)).
 
+% Player name is not in use
+player_free(_, []) -> true;
+player_free(Player, [{Name, _} | Players]) ->
+        if
+          Player =:= Name -> false;
+          true -> player_free(Player, Players)
+        end.
+
+inc_nth(_, []) -> [];
+inc_nth(N, [X | Xs]) ->
+        if 
+          N =:= 1 -> [X + 1 | Xs];
+          true -> [X | inc_nth(N-1, Xs)]
+        end.
+        
+% Builds a distribution of answers
+get_dist(Guesses, NumAnswers) ->
+        Initial = lists:duplicate(NumAnswers, 0),
+        Indexes = maps:values(Guesses),
+        Fun = fun(X, List) -> inc_nth(X, List) end,
+        lists:foldl(Fun, Initial, Indexes).
+
+
 hideAnswers({Description, Answers}) ->
         HideAnswer =
           fun(X) ->
               case X of
                       {_, Answer} -> Answer;
-                      Answer            -> Answer
+                      Answer -> Answer
               end
           end,
         {Description, lists:map(HideAnswer, Answers)}.
 
 % Increments the value for key Pid in Total by each score in a list of {Pid, Score}
 addToTotal([], Total) -> Total;
-addToTotal([{Pid, Score} | Scores], Total) ->
-        CurrentScore = maps:get(Pid, Total, 0),
-        NewTotal = maps:update(Pid, Score + CurrentScore, Total),
+addToTotal([{Ref, Score} | Scores], Total) ->
+        CurrentScore = maps:get(Ref, Total, 0),
+        NewTotal = maps:update(Ref, Score + CurrentScore, Total),
         addToTotal(Scores, NewTotal).
-
-% Player name is not in use
-name_free(_, []) -> true;
-name_free(Player, [{Name, _} | Players]) ->
-        if
-          Player =:= Name -> false;
-          true -> name_free(Player, Players)
-        end.
-
-
 
 %Need to add a pattern match for case of failure
 start() -> {ok, spawn(quizmaster, loop, [queue:new(), #{}, editable])}.
@@ -178,8 +232,15 @@ join(Q, Player) ->
         Q ! {self(), join, Player},
         receive
           {Q, {ok, Ref}} -> {ok, Ref};
-          {Q, {error, Error}} -> {error, Error}
+          {Q, {error, Reason}} -> {error, Reason}
         end.
 
-leave(Q, Player) -> "not implemented".
-guess(Q, Ref, Index) -> "not implemented".
+leave(Q, Ref) ->
+        Q ! {self(), leave, Ref},
+        receive
+          {Q, ok} -> ok;
+          {Q, {error, Reason}} -> {error, Reason}
+        end.
+
+guess(Q, Ref, Index) ->
+        Q ! {self(), guess, Ref, Index}.
