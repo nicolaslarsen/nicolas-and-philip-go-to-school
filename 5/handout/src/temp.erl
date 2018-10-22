2c43612b3b88fe76b3df92d3318c37eba886bc44
Philip Lassen <philiplassen@gmail.com>
2018-10-22 13:23:23 +0200

    silly test file

---
+++ 5/handout/src/temp.erl
@@ -0,0 +1,48 @@
+-module(quizmaster).
+
+%% API exports.
+-export([start/0, add_question/2, get_questions/1,
+         play/1, next/1, timesup/1, loop/2,
+         join/2, leave/2, guess/3]).
+
+%Need to add a pattern match for case of failure
+start() -> {ok, spawn(quizmaster, loop, [queue:new(), #{}])}.
+
+
+add_question(Q, {Description, MarkedAnswers}) -> 
+    Q ! {self(), {add_question, Description, MarkedAnswers}}.
+
+get_questions(Q) ->
+    Q ! {self(), get_questions},
+    receive
+      {Q, {questions, Questions}} -> queue:to_list(Questions)
+    end.
+
+
+
+loop(Questions, Players) ->
+    Me = self(),
+    receive
+        {From, {add_question, Description, MarkedAnswers}} ->
+            loop(queue:snoc(Questions, {Description, MarkedAnswers}), Players);
+        {From, get_questions} ->
+            From ! {Me, {questions, Questions}},
+            loop(Questions, Players);
+        _ -> "This failed"
+    end.
+
+play(Q) -> 
+    register(, self()),
+    case get_questions(Q) of 
+        [] -> {error, no_questions};
+        _ -> ok
+    end.
+
+
+
+
+next(Q) -> "not implemented".
+timesup(Q) -> "not implemented".
+join(Q, Player) -> "not implemented".
+guess(Q, Ref, Index) -> "not implemented".
+leave(Q, Player) -> "not implemented".
---
+++ 5/handout/src/test_quizmaster.erl
@@ -0,0 +1,51 @@
+ -module(test_quizmaster).
+   -export([fib/1, starter/0, questions_test_/0, pre_pop/0, next_test_/0]).
+   -include_lib("eunit/include/eunit.hrl").
+
+   fib(0) -> 1;
+   fib(1) -> 1;
+   fib(N) when N > 1 -> fib(N-1) + fib(N-2).
+
+   fib_test_() ->
+       [?_assert(fib(0) =:= 1),
+	?_assert(fib(1) =:= 1),
+	?_assert(fib(2) =:= 2),
+	?_assert(fib(3) =:= 3),
+	?_assert(fib(4) =:= 6),
+	?_assert(fib(5) =:= 8),
+	?_assertException(error, function_clause, fib(-1)),
+	?_assert(fib(31) =:= 2178309)
+       ].
+
+
+starter() ->
+    {_, A} = quizmaster:start(),
+    quizmaster:add_question(A, {"who is my partner", [{correct, "nicolas"}, "philip", "tom"]}),
+    quizmaster:add_question(A, {"who is my cool", [{correct, "Jason"}, "sam,", "lily"]}),
+    quizmaster:add_question(A, {"who is the prettiest", [{correct, "Philip"}, "tom", {correct, "Nicolas"}]}),
+    quizmaster:get_questions(A).
+
+
+pre_pop() ->
+    {_, A} = quizmaster:start(),
+    quizmaster:add_question(A, {"who is my partner", [{correct, "nicolas"}, "philip", "tom"]}),
+    quizmaster:add_question(A, {"who is my cool", [{correct, "Jason"}, "sam,", "lily"]}),
+    quizmaster:add_question(A, {"who is the prettiest", [{correct, "Philip"}, "tom", {correct, "Nicolas"}]}),
+    A.
+ 
+
+questions_test_() -> 
+    Result =  [{"who is my partner", [{correct, "nicolas"}, "philip", "tom"]},
+               {"who is my cool", [{correct, "Jason"}, "sam,", "lily"]},
+               {"who is the prettiest", [{correct, "Philip"}, "tom", {correct, "Nicolas"}]}],
+    [?_assert(starter() =:= Result)].
+
+
+next_test_() ->
+    A = pre_pop(),
+    quizmaster:play(A),
+    [?_assert(quizmaster.next(A) =:= {"who is my partner",["nicolas","philip","tom"]}),
+     ?_assert(quizmaster.next(A) =:= {"who is my cool", ["Jason", "sam,", "lily"]}),
+     ?_assert(quizmasater.next(A) =:= {"who is the prettiest", ["Philip", "tom", "Nicolas"]})].
+
+
