9f0c23be5cc6a7671e96f7906653550cbfed8529
nicolas <nicolasringsmose@hotmail.com>
2018-11-18 21:51:03 +0100

    end of the night

--- 2/handout/src/Parser/Impl.hs
+++ 2/handout/src/Parser/Impl.hs
@@ -15,46 +15,34 @@ data ParseError = ParseError String
 parseString :: String -> Either ParseError Expr
 parseString = undefined
 
-isLetter :: Char -> Bool
 isLetter char = (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')
 
-isDigit :: Char -> Bool
 isDigit char = char >= '0' && char <= '9'
 
-ident :: ReadP Expr
 ident = do
     val <- ident1 <|> ident2
-    return val    
+    return val
 
-ident1 :: ReadP Expr
 ident1 = do
     firstLetter <- satisfy isLetter
     remainder <- many1 (satisfy (\c -> isLetter c || isDigit c))
-    satisfy (== ' ')
     return (Var  ([firstLetter] ++  remainder))
 
-ident2 :: ReadP Expr
 ident2 = do
   letter <- satisfy isLetter
-  satisfy (== ' ')
   return (Var [letter])
-   
-number1 :: ReadP Expr 
-number1 = do 
+
+number1 = do
    firstDigit <- satisfy (\c -> isDigit c &&  c /= '0')
    remainder <- many1 (satisfy isDigit)
-   satisfy (== ' ')
    return (Number (read ([firstDigit] ++ remainder)::Int))
 
-number2 :: ReadP Expr
 number2 = do
    dig <- satisfy isDigit
-   satisfy (== ' ')
    return (Number (read [dig]::Int))
 
 
-number :: ReadP Expr
-number = do 
+number = do
    val <- number1 <|> number2
    return val
 
@@ -63,22 +51,21 @@ string1  word = do
    return (String result)
 
 trueReader = do
-  string "true "
+  string "true"
   return TrueConst
 
 falseReader = do
-  string "false "
+  string "false"
   return FalseConst
 
 undefinedReader = do
-  string "undefined "
+  string "undefined"
   return Undefined
 
-arrayFor = do 
-   string "for "
+arrayFor = do
+   string "for"
    string "("
    iden <- ident
-   string " "
    string "of"
    exp1 <- expr1
    string ")"
@@ -91,19 +78,17 @@ arrayBody = do
    result <- expr1
    return (ACBody result)
 
-arrayIf = do 
+arrayIf = do
    string "if"
-   string " "
    string "("
    exp1 <- expr1
    string ")"
-   string " "
    ac <- arraycompr
    return (ACIf exp1 ac)
 
 
 arraycompr = do
-   result <- arrayFor <|> arrayIf <|> arrayBody 
+   result <- arrayFor <|> arrayIf <|> arrayBody
    return result
 
 isInString char = (char >= '!' && char <= '~' && char /= '\\' && char /= '\'')
@@ -126,13 +111,32 @@ funReader = do
             <|> string "<" <|> string "==="
     exp2 <- expr1
     return (Call fun [exp1, exp2])
-        
+
+data Ex =
+        Expr
+      | Eof
+      deriving (Read, Show)
+
+eo = do
+    eof
+    return Eof
+
+exprs = do
+    eo <|> exprs2
+
+exprs2 = do
+    exp1  <- expr1
+    ce    <- commaExprs
+    case ce of
+      Eof  -> return exp1
+      _   -> return (Comma exp1, ce)
 
 expr = do
     val <- expr1 <|> commaExpr
     return val
- 
-expr1 = do 
+
+expr1 = do
    val <- trueReader <|> falseReader <|> ident <|> number <|>
-           undefinedReader <|> funReader
+           undefinedReader
+   skipSpaces
    return val
