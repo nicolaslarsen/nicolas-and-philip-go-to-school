7cc90127f96903f5f36c37357cf0dbe51ddf228a
Philip Lassen <philiplassen@gmail.com>
2018-11-28 23:04:44 +0100

    latest changes to ap

---
+++ 1/handin/.stack-work/dist/x86_64-osx/Cabal-2.2.0.1/build/subscript-interpreter/autogen/Paths_subscript_interpreter.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_subscript_interpreter (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/Users/philiplassen/CS/nicolas-and-philip-go-to-school/1/handin/.stack-work/install/x86_64-osx/lts-12.6/8.4.3/bin"
+libdir     = "/Users/philiplassen/CS/nicolas-and-philip-go-to-school/1/handin/.stack-work/install/x86_64-osx/lts-12.6/8.4.3/lib/x86_64-osx-ghc-8.4.3/subscript-interpreter-0.0.0-62mKoDhxkxA7ynOuJkhd9A-subscript-interpreter"
+dynlibdir  = "/Users/philiplassen/CS/nicolas-and-philip-go-to-school/1/handin/.stack-work/install/x86_64-osx/lts-12.6/8.4.3/lib/x86_64-osx-ghc-8.4.3"
+datadir    = "/Users/philiplassen/CS/nicolas-and-philip-go-to-school/1/handin/.stack-work/install/x86_64-osx/lts-12.6/8.4.3/share/x86_64-osx-ghc-8.4.3/subscript-interpreter-0.0.0"
+libexecdir = "/Users/philiplassen/CS/nicolas-and-philip-go-to-school/1/handin/.stack-work/install/x86_64-osx/lts-12.6/8.4.3/libexec/x86_64-osx-ghc-8.4.3/subscript-interpreter-0.0.0"
+sysconfdir = "/Users/philiplassen/CS/nicolas-and-philip-go-to-school/1/handin/.stack-work/install/x86_64-osx/lts-12.6/8.4.3/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "subscript_interpreter_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "subscript_interpreter_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "subscript_interpreter_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "subscript_interpreter_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "subscript_interpreter_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "subscript_interpreter_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
--- ap/4/handout/flamingo.erl
+++ ap/4/handout/flamingo.erl
@@ -1,56 +1,8 @@
--module(flamingo).
+-module(flamingo)
 
--export([new/1, request/4, route/4, drop_group/2, request_reply/2, loop/2, new_mapping/4]).
 
-request_reply(Pid, Request) ->
-    Pid ! {self(), Request},
-    receive
-        {Pid, Response} -> Response
-    end.
+-export([new/1, request/4, route/4, drop_group/2])
 
 
 
-new(_Global) -> {ok, spawn(flamingo, loop, [_Global, #{}])}.
-
-request(_Flamingo, _Request, _From, _Ref) ->
-    _Flamingo ! {_From, {request, _Request, _Ref}}.
-    %receive
-     %   {_Ref, Response} -> Response;
-      %  _ -> "this failed epically"
-    %end.
-
-route(_Flamingo, _Path, _Fun, _Arg) ->
-    _Flamingo ! {self(), {route, _Path, _Fun, _Arg}},
-    receive
-        {ok, _Flamingo} -> {ok, _Flamingo};
-        _ -> "we super failed"
-    end.
-
-drop_group(_Flamingo, _Id) ->
-    not_implemented.
-
-
-loop(State, LocalState) ->
-    Me = self(),
-    receive
-        {From, {request, {_Path, _Args}, _Ref}} ->
-            case maps:find(_Path, LocalState) of
-              {ok, Fun} -> 
-                From ! {_Ref, {200, apply(Fun, [{_Path, _Args}, State, LocalState])}};
-              {error} -> 
-                From ! {_Ref, {404, "Path not found"}}
-            end,
-            loop(State, LocalState);
-        {From, {route, Path, Fun, Args}} ->
-            NewMap = new_mapping(Path, Fun, Args, LocalState),
-            From ! {ok, Me},
-            loop(State, NewMap);
-        _ -> "we Failed"
-    end.
-
-new_mapping(Path, Fun, Args, Map) ->
-    case (Path) of
-        [H|T] -> new_mapping(T, Fun, Args, Map#{H => Fun});
-        [] -> Map
-    end.
-
+new(_Global) -> {ok, spawn(flamingo, loop, [_Global, #{}])}
