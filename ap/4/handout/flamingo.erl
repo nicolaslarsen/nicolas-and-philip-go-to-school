21fdae44f3ebcd605c75b3365ecda282d912f7ca
Philip Lassen <philiplassen@gmail.com>
2018-11-29 04:32:14 +0100

    I have working implementations of route and new

--- ap/4/handout/flamingo.erl
+++ ap/4/handout/flamingo.erl
@@ -1,8 +1,39 @@
--module(flamingo)
+-module(flamingo).
 
 
--export([new/1, request/4, route/4, drop_group/2])
+-export([new/1, test/1, route/4, request/,loop/3]).
 
 
 
-new(_Global) -> {ok, spawn(flamingo, loop, [_Global, #{}])}
+new(_Global) -> {ok, spawn(flamingo, loop, [_Global, #{}, #{}])}.
+
+route(Flamingo, Prefixes, Action, Init) ->
+  Flamingo ! {self(), {route, Prefixes, Action, Init}},
+  receive 
+    {ok, Ref} -> {ok, Ref}
+  end.
+
+test(Flamingo) -> 
+  Flamingo ! {self(), {test}},
+  receive
+    {results, RouteMap, StateMap} -> {RouteMap, StateMap}
+  end.
+
+loop(State, RouteMap, StateMap) ->
+  %Me = self(),
+  receive
+    {From, {route, Prefixes, Action, Init}} ->
+      Ref = make_ref(),
+      Pairs = lists:zip(Prefixes, lists:duplicate(length(Prefixes), Ref)),
+      Fun = fun(K, V, AccIn) -> maps:put(K, V, AccIn) end,
+      NewRouteMap = maps:fold(Fun, RouteMap, maps:from_list(Pairs)),
+      NewStateMap = maps:put(Ref, {Action, Init}, StateMap),
+      From ! {ok, Ref},
+      loop(State, NewRouteMap, NewStateMap);
+    {From, {test}} ->
+      From ! {results, RouteMap, StateMap},
+      loop(State, RouteMap, StateMap)
+  end.
+
+request(Flamingo, Request, From, Ref) ->
+  
